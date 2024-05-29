51de2dc1e54c456228676afa653bcaf01dc66280
Cizok <tarcisioneves98@gmail.com>
2024-05-28 22:23:30 -0300

    AtvSpring

---
+++ README.md
@@ -0,0 +1,1 @@
+"# AtvSpring02" 
---
+++ src/test/java/br/com/teste/testspring/TestspringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.teste.testspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
